6c431c8a62e32085c0c7b0c8fc00bf8dbdc3fb7e
tao.chen <tao.chen@ishandian.net>
2018-12-22 14:00:52 +0800

    积分物品列表页面 积分物品详情页面 积分物品精品推荐API 积分物品兑换页面

--- application/homeapi/controller/Integral.php
+++ application/homeapi/controller/Integral.php
@@ -75,7 +75,7 @@ class Integral extends Base {
      * @Autor: 胡宝强
      * Date: 2018/8/8 15:56
      */
-    public function search()
+    public function integral_mall_list()
     {
         //验证参数
         !empty(I('sales_sum', '')) && !in_array($sales_sum = I('sales_sum', ''),['asc','desc']) && $this->errorMsg(2002, 'sales_sum');//选传
@@ -96,6 +96,133 @@ class Integral extends Base {
         $this->assign('data', $data);
 //        $this->json("0000", "加载成功", $data);
         return $this->fetch('dist/integral-mall-list');
+    }
+
+    /**
+     * 积分物品详情介绍页面
+     * @return mixed
+     */
+    public function integral_mall_detail()
+    {
+        //验证参数
+        $goods_id = I('goods_id');
+        empty($goods_id) && $this->errorMsg(2002, 'goods_id');//选传
+        //检索条件
+        $where['goods_id'] = $goods_id;
+        $where['exchange_integral'] = 2;
+
+        $Goods = new Goods();
+        $field = "goods_id,goods_name,goods_remark,original_img,is_recommend,is_new,is_hot,type,integral,moren_integral,sales_sum";
+        $data = $Goods->GoodsList($this->page, 0, $where, [], 1, $field);
+
+        //加载商品轮播
+        $banner = (new GoodsImages())->getImage($goods_id);
+        $data['banner'] = $banner;
+
+        $this->assign('detail', $data);
+
+        $goods_where['is_recommend'] = 1;           //推荐商品
+        $goods_where['exchange_integral'] = 2;      //纯积分商品
+        $recommend_car = $Goods->GoodsList(1,'',$goods_where,['sort'=>'desc'],6,'goods_id,goods_name,goods_remark,sales_sum,original_img,label,integral,moren_integral,type,store_count');
+        $data['recommend_car'] = $recommend_car;
+        $this->assign('recommend_car', $recommend_car);
+
+//        $this->json(200, 'ok', $data);
+        return $this->fetch('dist/integral-mall-detail');
+    }
+
+    /**
+     * 积分物品——精品推荐列表——API
+     */
+    public function integral_mall_recommend()
+    {
+        $Goods = new Goods();
+
+        $goods_where['is_recommend'] = 1;           //推荐商品
+        $goods_where['exchange_integral'] = 2;      //纯积分商品
+        $recommend_car = $Goods->GoodsList($this->page,'',$goods_where,['sort'=>'desc'],6,'goods_id,goods_name,goods_remark,sales_sum,original_img,label,integral,moren_integral,type,store_count');
+        $data['recommend_car'] = $recommend_car;
+        $this->json(200, 'ok', $recommend_car);
+    }
+
+
+    /**
+     * 积分物品兑换页面
+     */
+    public function integral_mall_exchange()
+    {
+        $data = $this->get_goods_detail();
+        $this->assign('data', $data);
+        $this->json(200, 'ok', $data);
+        return $this->fetch('dist/integral-mall-exchange');
+    }
+
+    private function get_goods_detail()
+    {
+        !empty(I('goods_id', '')) && !is_numeric($goods_id = I('goods_id', 0)) && $this->errorMsg(2002, 'goods_id');//必传
+        $Goods = new Goods();
+
+        $GoodsLogic = new GoodsLogic();
+        $SonOrderComment = new SonOrderComment();
+        //增加点击数
+        $Goods->addClickCount($goods_id);
+        //加载商品轮播
+        $banner = (new GoodsImages())->getImage($goods_id);
+        //$this->assign('banner', $banner);
+        $data['banner'] = $banner;
+
+        //商品详情
+        $where['goods_id'] = $goods_id;
+        $field = "goods_id,goods_name,equity_content,label,equity_desc,goods_remark,price,
+        deposit_price,store_count,sales_sum,goods_content,integral,exchange_integral,
+        integral_money as integrals_moneys,video";
+        $data = $Goods->GoodsList($this->page, 1, $where, [], 1, $field);
+        $data->equity_desc = str_replace("", "<br/>", $data->equity_desc);
+        $data->equity_desc = str_replace(" ", "&nbsp;", $data->equity_desc);
+        $data->equity_content = str_replace("", "<br/>", $data->equity_content);
+        $data->equity_content = str_replace(" ", "&nbsp;", $data->equity_content);
+        $data->goods_content = htmlspecialchars_decode('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"></head><body><style>#goods_info_content_div p{margin:0px; padding:0px} #goods_info_content_div p img{width:100%} </style><div id="goods_info_content_div">' . $data->goods_content . '</div></body></html>');
+        //价格表
+        $price_list = $GoodsLogic->priceList($goods_id);
+        //$this->assign('price_list', $price_list);
+        $data['price_list'] = $price_list;
+
+        //求这个商品需要多少积分
+//        $exchange_integral = I('exchange_integral')??0;
+        if($data['exchange_integral'] == 0){        //纯现金
+            $data['most_point'] = 0;
+            $data['minimum_point'] = 0;
+        }elseif($data['exchange_integral'] == 1){   //积分+现金
+            $data['most_point'] = $data['integral'];
+            $data['minimum_point'] = 1;
+        }elseif($data['exchange_integral'] == 2){   //纯积分
+            $data['most_point'] = $data['integral'];
+            $data['minimum_point'] = $data['integral'];
+        }
+        $point_rate = tpCache('shopping.point_rate'); //兑换比例
+        $data['integral_money'] = round($data['most_point']/$point_rate,2);  //积分最高可以抵多少钱
+
+        //获取这个用户还有多少积分
+        if(I('token')){
+            $user_id = $this->checkToken(I('token'));
+            $data['userIntegral'] = getIntegral($user_id);
+        }
+
+        $data["banner"] = $banner;
+        $data["price_list"] = $price_list;
+
+        $data['spec'] = $GoodsLogic->get_sku($goods_id);//外观颜色
+        $appearance['displacement'] = $GoodsLogic->get_sku($goods_id, $data['spec'][0]['id'], 'displacement');//排量
+        $appearance['model'] = $GoodsLogic->get_sku($goods_id, $appearance['displacement'][0]['id'], 'model');//型号
+        $appearance['interior'] = $GoodsLogic->get_sku($goods_id, $appearance['model'][0]['id'], 'interior');//内饰颜色
+        $appearance['province'] = $GoodsLogic->get_sku($goods_id, $appearance['interior'][0]['id'], 'province');//城市
+        $appearance['city'] = $GoodsLogic->get_sku($goods_id, $appearance['province'][0]['id'], 'city', $appearance['interior'][0]['id']);//城市
+        $appearance['distribu'] = $GoodsLogic->get_sku($goods_id, $appearance['city'][0]['id'], 'distribu', $appearance['interior'][0]['id']);//经销商
+        $data['appearance'] = $appearance;
+        $data['comment'] = $SonOrderComment->commentList($this->page,$goods_id,2);
+        $data['comment_count'] = $SonOrderComment->count;
+
+        return $data;
     }
 
 }
